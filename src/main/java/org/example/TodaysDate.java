2b7522320fe60814e1b346b29302045865bf1d00
mjosepc <mjose.powell@gmail.com>
2023-10-03 18:41:02 -0300

    1 commit

---
+++ src/main/java/org/example/TodaysDate.java
@@ -0,0 +1,31 @@
+package org.example;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class TodaysDate {
+    // Nombrar variables
+    private String hora;
+    private int day;
+    private int month;
+    private int year;
+
+    // Métodos
+    public void imprimirFechayHora() {
+        GregorianCalendar calendar = new GregorianCalendar();
+        this.hora = calendar.get(Calendar.HOUR_OF_DAY) + ":" + calendar.get(Calendar.SECOND);
+        this.day = calendar.get(Calendar.DATE);
+        this.month = calendar.get(Calendar.MONTH) + 1;
+        this.year = calendar.get(Calendar.YEAR);
+
+        System.out.println("Hora: " + this.hora);
+        System.out.println("Fecha: " + this.day+ " " + this.month + " " + this.year + " ");
+    }
+
+    public static void main(String[] args) {
+        TodaysDate today = new TodaysDate();
+        today.imprimirFechayHora();
+    }
+}
+
+
+
---
+++ src/test/java/org/example/TodaysDateTest.java
@@ -0,0 +1,14 @@
+package org.example;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TodaysDateTest {
+
+    @Test
+    void imprimirFechayHora() {
+
+        TodaysDate today = new TodaysDate();
+        today.imprimirFechayHora();
+
+    }
+ }
